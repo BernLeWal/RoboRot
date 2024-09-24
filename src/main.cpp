0176805001188656ffc7da721e44692ed28bfe03
wallisch <walliscb@technikum-wien.at>
2024-09-24 19:32:17 +0200

    manual stepper motor test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
 #include <Arduino.h>
+
+#include "setup.h"
+
+
 void setup() {
     // write your initialization code here
     Serial.begin(115200);
+    Serial.print("RoboROT Firmware ");
+    Serial.println(APP_VERSION);
+
     // initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
+
+    // initialize the stepper driver
+    pinMode(MOTOR1_DIR_PIN, OUTPUT);
+    pinMode( MOTOR1_PUL_PIN, OUTPUT);
 }
 
+
+bool blink = false;
+
 void loop() {
-// write your code here
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    Serial.println("LED is on");
-    delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    Serial.println("LED is off");
-    delay(1000);                       // wait for a second
+    // LED
+    blink = !blink;
+    Serial.print(blink ? "LED is on " : "LED is off");
+    digitalWrite(LED_BUILTIN, blink ? HIGH : LOW);   // switch the LED on/off (HIGH is the voltage level)
+    Serial.print(" - ");
+
+    // Stepper Motor
+    digitalWrite( MOTOR1_DIR_PIN, blink ? HIGH : LOW);
+    for (int i=0; i<1600; i++) {
+        digitalWrite(MOTOR1_PUL_PIN, HIGH);
+        delayMicroseconds(1000);
+        digitalWrite(MOTOR1_PUL_PIN, LOW);
+        delayMicroseconds(1000);
+    }
+
+    Serial.println();
+    delay(100);                       // wait for a second
 }
+
---
+++ src/setup.h
@@ -0,0 +1,19 @@
+//
+// Contains the hardware and I/O-setup of the microcontroller board
+// Created by wallisch on 24.09.2024.
+//
+
+#ifndef ROBOROT_SETUP_H
+#define ROBOROT_SETUP_H
+
+#define APP_VERSION "V0.1.0"
+
+#define MOTOR_DEFAULT_MAX_SPEED 200
+#define MOTOR_DEFAULT_ACCELERATION 100
+
+// The bottommost motor
+#define MOTOR1_TYPE 1
+#define MOTOR1_DIR_PIN 22
+#define MOTOR1_PUL_PIN 23
+
+#endif //ROBOROT_SETUP_H
